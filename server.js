210b21e4803d5d8a94c2b707ab22ae7f627e3041
arwebber <andywebber96@gmail.com>
2021-09-26 18:01:11 -0500

    use reverse proxy in express

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+const localServer = 'http://localhost:3001';
+const prodServer = 'https://event-checkout-api.herokuapp.com';
+
 const app = express();
+
+app.all("/api/*", function (req, res) {
+    console.log('redirecting to Server1');
+    apiProxy.web(req, res, { target: prodServer });
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/event-checkout'));
