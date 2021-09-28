63d02f7a82ba90e42fe6538f497888fb488969d9
arwebber <andywebber96@gmail.com>
2021-09-27 22:05:03 -0500

    fixed type for models

---
+++ src/app/models/cart-contents.model.ts
@@ -0,0 +1,27 @@
+export class CartContentsModel {
+  cart_item_id: string;
+  cart_id: number;
+  date_created: string;
+  event_id: number;
+  price: number;
+  title: string;
+  quantity: string;
+
+  constructor(
+    cart_item_id: string,
+    cart_id: number,
+    date_created: string,
+    event_id: number,
+    price: number,
+    title: string,
+    quantity: string
+  ) {
+    this.cart_item_id = cart_item_id;
+    this.cart_id = cart_id;
+    this.date_created = date_created;
+    this.event_id = event_id;
+    this.price = price;
+    this.title = title;
+    this.quantity = quantity;
+  }
+}
