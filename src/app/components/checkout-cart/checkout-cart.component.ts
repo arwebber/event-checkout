3c85bf1c340284894ccff1392b0cf9b79f184134
arwebber <andywebber96@gmail.com>
2021-09-27 22:04:32 -0500

    add checkout services

--- src/app/components/checkout-cart/checkout-cart.component.ts
+++ src/app/components/checkout-cart/checkout-cart.component.ts
@@ -1,17 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { CartContentsModel } from 'src/app/models/cart-contents.model';
+import { CheckoutService } from './services/checkout.service';
 
 @Component({
   selector: 'app-checkout-cart',
   templateUrl: './checkout-cart.component.html',
   styleUrls: ['./checkout-cart.component.css']
 })
-export class CheckoutCartComponent implements OnInit {
+export class CheckoutCartComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  constructor(
+    private checkoutService: CheckoutService,
+    private cookieService: CookieService
+  ) { }
 
+  /**
+   * List of cart contents.
+   */
+  cartContentList: CartContentsModel[]
+
+  /**
+   * Boolean to determine if the promo textbox should be shown.
+   */
   promoCode = false;
 
+  /**
+   * Boolean to determine if edit has been selected.
+   */
+  editMode = false;
+
+  /**
+   * Boolean to determine if edit has been selected.
+   */
+  subtotal: number;
+
   ngOnInit(): void {
+    const sessionId = this.cookieService.get('session_id')
+    if (sessionId != null) {
+      this.checkoutService.getCartBySession(sessionId).then((cart) => {
+        this.cartContentList = cart;
+      });
+
+      this.cartSubTotal(sessionId);
+    }
+  }
+
+  /**
+   * Function to delete item from cart.
+   */
+  deleteItem(cartItemId) {
+    console.log('clicked!!!', cartItemId)
+    this.checkoutService.deleteCartItemById(cartItemId).then(() => {
+      const obj = this.cartContentList.filter((item) => {
+        return item.cart_item_id == cartItemId;
+      })
+      this.cartContentList.splice(this.cartContentList.indexOf(obj[0]), 1);
+    });
+  }
+
+  cartSubTotal(sessionId) {
+    this.checkoutService.getCartSubtotal(sessionId).then((subtotal) => {
+      console.log('sub', subtotal.subtotal);
+      this.subtotal = subtotal.subtotal;
+    })
+  }
+
+  ngOnChanges(): void {
+    console.log('change occured in checkout')
   }
 
 }
---
+++ src/app/components/checkout-cart/services/checkout.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { CartContentsModel } from 'src/app/models/cart-contents.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Return the cart for a given session.
+   * @param sessionId
+   * @return cart
+   */
+  getCartBySession(sessionId: string) {
+    console.log('time to get cart by session', sessionId);
+    return new Promise<CartContentsModel[]>((resolve, reject) => {
+      this.http
+        .get('/api/cart/v1/contents/by/session/', { params: { sessionId } })
+        .subscribe((cart: CartContentsModel[]) => {
+          resolve(cart);
+        });
+    });
+  }
+
+
+  getCartSubtotal(sessionId: string) {
+    return new Promise<any>((resolve, reject) => {
+      this.http
+        .get('/api/cart/v1/contents/total/', { params: { sessionId } })
+        .subscribe((subtotal) => {
+          resolve(subtotal);
+        });
+    });
+  }
+
+  /**
+   * Delete an item from the cart.
+   */
+  deleteCartItemById(cartItemId: number) {
+    return new Promise<any>((resolve, reject) => {
+      this.http
+        .delete('/api/cart/v1/delete/cart/item/', { body: { cart_item_id: cartItemId } })
+        .subscribe((cart) => {
+          resolve(cart);
+        });
+    });
+  }
+}
